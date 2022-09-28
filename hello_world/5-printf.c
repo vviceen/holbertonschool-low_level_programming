4c0fa3e07e627eed254cc7d29c646d32aafda9f0
vviceen <debrunvicente@gmail.com>
2022-09-28 10:44:44 -0700

    5printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Use the function "printf"
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
