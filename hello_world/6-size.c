3d2e64dc024a3358f7638e397cc5459605ff7cfc
vviceen <debrunvicente@gmail.com>
2022-09-28 12:00:00 -0700

    6malditofix

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -16,9 +16,10 @@ int main(void)
 
 	printf("Size of char: %zu byte\n", sizeof(caracter));
 	printf("Size of int: %zu bytes\n", sizeof(entero));
-	printf("Size of long int: %zu bytes\n", sizeof(longint);
+	printf("Size of long int: %zu bytes\n", sizeof(longint));
 	printf("Size of long long int: %zu bytes\n", sizeof(longie));
 	printf("Size of float: %zu bytes\n", sizeof(floaty));
 	
-	return: 0;
+	return (0);
+
 }
