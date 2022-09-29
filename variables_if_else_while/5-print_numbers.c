47066874395a55f14e52a5f1e3582a66c720cbb4
vviceen <debrunvicente@gmail.com>
2022-09-29 14:32:53 -0700

    5printnumbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lowercase with a control structure
+ *
+ *i Return: 0
+ *
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar (n);
+	}
+	putchar ('\n');
+	return (0);
+}
