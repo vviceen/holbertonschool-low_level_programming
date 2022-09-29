3267a4c4e4385d5055d4f550cddfdf8ce8de8216
vviceen <debrunvicente@gmail.com>
2022-09-29 13:39:34 -0700

    2printlowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lowercase with a control structure
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar (n);
+	}
+	putchar ('\n');
+	return (0);
+}
