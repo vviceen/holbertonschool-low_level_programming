54012e6ca7847c7ebc09f52227e825bdc04664de
vviceen <debrunvicente@gmail.com>
2022-09-29 14:38:50 -0700

    6printz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print numberse with a control structure
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar (n);
+	}
+	putchar ('\n');
+	return (0);
+}
