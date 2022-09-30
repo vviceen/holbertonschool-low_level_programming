e1ea0d16f706848ce83324bf213b2eb79ed0b8fe
vviceen <debrunvicente@gmail.com>
2022-09-30 11:54:06 -0700

    a

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * main - prints _putchar
  *
@@ -7,15 +7,15 @@
 
 int main(void)
 {
-	putchar('-');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef main_h
+#define main_h
+#include <unistd.h>
+#include <stdio.h>
+
+int _putchar(char c)
+
+#endif
