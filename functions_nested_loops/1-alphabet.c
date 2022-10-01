1c21f748fc4d16a2cc763ffe5541f5448dc15221
vviceen <debrunvicente@gmail.com>
2022-10-01 11:00:37 -0700

    1alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet
+ *
+ */
+
+void print_alphabet(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar (n);
+	}
+	_putchar ('\n');
+}
