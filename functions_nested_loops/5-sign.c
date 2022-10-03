71a42a964627edb52449bdaad0bc0d51fe0dd9d8
vviceen <debrunvicente@gmail.com>
2022-10-03 10:22:09 -0700

    5firstcheck

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - print alphabet
+ * @n: character to be checked
+ * Return: 1 if greater than 0, 0 if is 0, -1 else
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+	else
+	{
+		_putchar ('-');
+		return (-1);
+	}
+}
