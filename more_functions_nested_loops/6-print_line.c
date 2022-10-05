4942ce55e06a4ecac5d285073d733e0040e8c809
vviceen <debrunvicente@gmail.com>
2022-10-05 12:09:01 -0700

    6printline

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - checks for digit.
+ * @n: number of '_'
+ * Return: draes a straight line in terminal
+ */
+
+void print_line(int n)
+{
+	for (; n = 0; n--)
+	{
+		_putchar ('_');
+	}
+	if (n < 0)
+	{
+		_putchar ('\n');
+	}
+}
