1f9ed421143b8c40dd3f6ae00ea1abeaf14b2b2c
vviceen <debrunvicente@gmail.com>
2022-10-05 06:09:35 -0700

    2mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - checks for uppercase character.
+ * @a: int multiplier.
+ * @b: int multiplier.
+ * Return: a * b.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
