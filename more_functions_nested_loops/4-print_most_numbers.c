fc4fe23de8421dab0cd0dae997030c39bc29186d
vviceen <debrunvicente@gmail.com>
2022-10-05 07:26:38 -0700

    4printmstnum

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - checks for digit.
+ * Return: prints numbers from 0 to 9, do not print 2 & 4
+ */
+
+void print_most_numbers(void)
+{
+	char n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		if (n != 50 && n!= 52)
+		{
+			_putchar (n);
+		}
+	}
+	_putchar ('\n');
+}
