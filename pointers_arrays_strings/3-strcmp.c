72551630ce74b7e95cb017fa88f7759759ff7967
vviceen <debrunvicente@gmail.com>
2022-10-12 14:08:03 -0700

    3strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcmp - compares two string.
+ * @s1: first string.
+ * @s2: second string.
+ * Return: int.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	for (i = 0 ; s1[i] - s2[i] == 0 && s1[i] != '\0' ; i++)
+		;
+		return (s1[i] - s2[i]);
+}
