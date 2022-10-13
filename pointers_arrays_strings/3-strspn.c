3391028d9cc823a459fbfe246810048a1fc4df26
vviceen <debrunvicente@gmail.com>
2022-10-13 12:57:45 -0700

    3strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strspn - return number of bytes of init seg
+ * @accept: pointer
+ * @s: pointer
+ * Return: dest
+ **/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (!accept[j])
+			break;
+	}
+	return (i);
+}
