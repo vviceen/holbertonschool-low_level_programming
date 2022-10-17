1812047e42f7f0e499f29eba3932b1c42b9e29e4
vviceen <debrunvicente@gmail.com>
2022-10-17 12:16:42 -0700

    2strlen

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string.
+ * Return: len.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return(_strlen_recursion(s + 1) + 1);
+	}
+	return (0);
+}
