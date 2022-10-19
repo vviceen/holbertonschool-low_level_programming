ff6336989ca98ead5a8163a9fb8194166f6085f3
vviceen <debrunvicente@gmail.com>
2022-10-18 19:02:06 -0700

    4add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers
+ * @argc:adds positive numbers
+ * @argv: qwd
+ * Return: dqw
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum;
+
+	sum = 0;
+
+	for (i = 1; i < argc ; i++)
+	{
+		for (j = 0; argv[i][j] != '\0' ; j++)
+		{
+			if (argv[i][j] < 47 || argv[i][j] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
