c0afaef39870696190d9c1bd85d34f69c426a056
vviceen <debrunvicente@gmail.com>
2022-10-18 17:57:29 -0700

    3mul

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: argument count
+ * @argv: argument list
+ * Return: an int
+ */
+
+int main(int argc, char **argv)
+{
+	int num1, num2, rslt;
+
+	if (arcg != 3)
+	{
+		printf("Error\n");
+		exit (EXIT_FAILURE);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	rslt= num1 * num2;
+	printf("%d\n", rslt);
+	exit (EXIT_SUCCESS);
+}
