e5947944864a303f270a7abef1801b4799c87aa8
vviceen <debrunvicente@gmail.com>
2022-10-25 11:39:34 -0700

    2stingconcat

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * *string_concat - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @s1: string 1.
+ * @s2: string 2.
+ * @n: number of bytes
+ * Return: cat wich is the concatenation of s1 and s2
+ */
+char *string_concat(char *s1, char *s2, unsigned int n)
+{
+	char *cat;
+	int l;
+	int j;
+	int i;
+	int k;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (l = 0; s1[l] != '\0'; l++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	cat = malloc(((l + j) + 1) * sizeof(char));
+
+	if (cat == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		cat[i] = s1[i];
+	}
+	for (k = 0; s2[k] != '\0' && k < n; k++)
+	{
+		cat[i] = s2[k];
+		i++;
+	}
+	return (cat);
+}
