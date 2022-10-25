fed5d89611cd537bf7f020c8f1079c8806768e89
vviceen <debrunvicente@gmail.com>
2022-10-25 11:07:42 -0700

    0malloc

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,5 +1,8 @@
 #ifndef main_h
 #define main_h
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
