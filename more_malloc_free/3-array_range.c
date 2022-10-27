20db47128bc7096b9ec88f4533b99ed921bc5b56
vviceen <debrunvicente@gmail.com>
2022-10-26 18:01:03 -0700

    3array

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * *array_range - creates an array of integers.
+ * @min: all the values from min.
+ * @max: all the vales from max.
+ * Return: fall NULL, successful ptr.
+ */
+int *array_range(int min, int max)
+{
+	char *ptr = NULL;
+	int i;
+	int n = (max - min + 1);
+
+	if (min > max)
+	{
+		return (ptr);
+	}
+	ptr = malloc(n);
+	if (!ptr)
+	{
+		return (ptr);
+	}
+	for (i = 0; i < n; i++)
+	{
+		ptr[i] = min;
+		min++;
+	}
+	return (ptr);
+}
+
