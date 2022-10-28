1d2673dc7a5116af0a5dc494913a7b8fefedb9ac
vviceen <debrunvicente@gmail.com>
2022-10-28 12:16:25 -0700

    betty fix

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef dog_h
 #define dog_h
+/**
+ * dog - the structure that has the data of the dog.
+ * @name: name the dog.
+ * @age: age the dog.
+ * @owner: the dog.
+ */
 struct dog
 {
 char *name;
