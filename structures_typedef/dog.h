8a88504b1d8c37817bead3e214e63152809001f9
vviceen <debrunvicente@gmail.com>
2022-10-31 10:56:35 -0700

    3newtype

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,12 +9,12 @@
  * @age: age the dog.
  * @owner: the dog.
  */
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+}dog_t;
 /* PROTOYPES */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
