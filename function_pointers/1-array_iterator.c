e57327fe53ac6efa0d3ef44230e2f63bacb577bc
vviceen <debrunvicente@gmail.com>
2022-11-01 13:49:08 -0700

    1arrayiterator

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - array iterator
+ * @array: array
+ * @size: size
+ * @action: function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	while (array[i] && action && i < (int)size)
+	{
+		action(*action);
+		i++;
+	}
+}
