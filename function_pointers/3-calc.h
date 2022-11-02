9dfe353d8752463a54aa3dfa63fd2123e0c64533
vviceen <debrunvicente@gmail.com>
2022-11-01 17:08:53 -0700

    fix

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,3 +1,5 @@
+#ifndef calc_h
+#define calc_h
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,3 +16,18 @@ typedef struct op
     char *op;
     int (*f)(int a, int b);
 } op_t;
+/*prototype function that obtains the function according to the operator*/
+int (*get_op_func(char *s))(int, int);
+
+/*prototypes of the functions that perform the operations*/
+int op_add(int a, int b);
+
+int op_sub(int a, int b);
+
+int op_mul(int a, int b);
+
+int op_div(int a, int b);
+
+int op_mod(int a, int b);
+
+#endif
