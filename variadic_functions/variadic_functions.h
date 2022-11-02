0b41476ae4b8decac9d7eb2a14925a752bbb52f3
vviceen <debrunvicente@gmail.com>
2022-11-01 17:48:46 -0700

    betty fix

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -14,7 +14,9 @@ void print_strings(const char *separator, const unsigned int n, ...);
 
 /*prototypes tas 3*/
 void print_all(const char * const format, ...);
-
+/**
+ * struct formats - function
+ */
 typedef struct formats
 {
 	char c;
