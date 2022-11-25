56dde36107d381b29031367e08c530629bced28b
vviceen <debrunvicente@gmail.com>
2022-11-25 15:32:11 -0800

    Betty fix

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * read_textfile -  a function that reads a text file and prints it
- * to the POSIX standard output.
- * filename: the file.
- * letters: is the number of letters it should read and print.
+ * read_textfile - a function that reads a text file
+ * @filename: the file.
+ * @letters: is the number of letters it should read and print.
+ *
  * Return: the actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
