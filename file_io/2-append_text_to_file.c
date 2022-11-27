4337f0c3f2e4664d2e022bee49a568f4d37a6791
vviceen <debrunvicente@gmail.com>
2022-11-27 15:01:40 -0800

    all

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * create_file - creates a file
+ * append_text_to_file - appends text at the end of a file
  * @filename: file name
- * @text_content: the string to write
+ * @text_content: string to write
  *
- * Return: 1 on success, -1 on failure
+ * Return: 1 on success and -1 on failure
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	ssize_t fd = 0, wr = 0;
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
---
+++ file_io/3-cp.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * main - copies the content of a file to another file.
+ * @argc: argument counter
+ * @argv: arguments:
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	ssize_t fd = 0, ft = 0, rd = 0, wr = 0;
+	char buff[BUFF_SIZE];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	ft = open(argv[2], O_WRONLY |  O_CREAT | O_TRUNC, 0664);
+	if (ft == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	while ((rd = read(fd, buff, BUFF_SIZE)))
+	{
+		if (rd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		wr = write(ft, buff, rd);
+		if (wr == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+	if ((close(fd) == -1))
+		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", fd), exit(100);
+	if ((close(ft) == -1))
+		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", ft), exit(100);
+	return (0);
+}
