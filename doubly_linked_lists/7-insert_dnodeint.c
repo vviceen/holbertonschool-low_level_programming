375f888488e1d40a647bf4823e9e3adfdd5cdad8
vviceen <debrunvicente@gmail.com>
2022-11-27 15:48:37 -0800

    fix

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,25 +1,38 @@
 #include "lists.h"
 /**
- * insert_dnodeint_at_index - insert node at given position
- * @h: head of the list
- * @idx: position to insert the node
- * @n:  number to insert in node
- *
- * Return: address of the new node, or NULL if it failed
+ * insert_dnodeint_at_index - return a node
+ * @h: linked liist
+ * @idx: position of node to be added
+ * @n: int value to node
+ * Return: new head node data or 0 if linked list is empty
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *nth = NULL, *node = *h;
+	dlistint_t *h1 = *h;
+	dlistint_t *pointerchain = NULL;
+	unsigned int i = 0;
 
-	if (!h)
+	pointerchain = malloc(sizeof(dlistint_t));
+	if (!pointerchain)
 		return (NULL);
-	if (!idx)
+	pointerchain->n = n;
+
+	if (idx == 0 || *h == NULL)
 		return (add_dnodeint(h, n));
-	node = get_dnodeint_at_index(*h, idx - 1);
-	if (!node)
-		return (NULL);
-	nth = add_dnodeint(&(node->next), n);
-	nth->prev = node;
-	node->next = nth;
-	return (node->next);
+	for (i = 0; i < idx - 1; i++)
+	{
+		if (!h1)
+			return (NULL);
+		h1 = h1->next;
+	}
+	if (h1->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	pointerchain->next = h1->next;
+	pointerchain->prev = h1;
+	h1->next = pointerchain;
+	h1 = pointerchain->next;
+	h1->prev = pointerchain;
+	return (pointerchain);
 }
